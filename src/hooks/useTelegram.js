5c428e37595e4dd056f00d00592301ed166d27a8
naithy <naithyDev@gmail.com>
2023-10-22 23:45:00 +0400

    fix collection data from tg #5

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -3,7 +3,7 @@ const tg = window.Telegram.WebApp;
 export function useTelegram() {
     return {
         tg,
+        photo_url: tg.initDataUnsafe.user.photo_url,
         user: tg.initDataUnsafe?.user,
-        photo_url: tginitDataUnsafe.user.photo_url,
     }
 }
