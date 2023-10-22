df9284d93f0725917a02c1773b7380d7a9620200
naithy <naithyDev@gmail.com>
2023-10-23 01:21:00 +0400

    tg ready

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Home.module.css'
 import Layout from '../../ui/Layout/Layout'
 import { useTelegram } from '../../../hooks/useTelegram'
@@ -9,6 +9,11 @@ tg.setHeaderColor('secondary_bg_color')
 
 
 const Home = () => {
+
+  useEffect(() => {
+    tg.ready();
+  }, [])
+
   return (
     <div className={styles.home_page}>
       <Layout/>
